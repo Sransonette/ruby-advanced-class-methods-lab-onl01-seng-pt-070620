77ed191535dc0d2f034de22e4e63410b2f5b72d6
Sean Ransonette <sransonette@gmail.com>
2020-07-28 03:02:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ class Song
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
-    @all << song
+    @@all << song
   end
 
 end
