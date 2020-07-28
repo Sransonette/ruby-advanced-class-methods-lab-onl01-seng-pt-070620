f4c62de0e205e0c3aa9646396f0d6a8df8901345
Sean Ransonette <sransonette@gmail.com>
2020-07-28 02:53:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,41 @@ class Song
 
   def save
     self.class.all << self
+  end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil 
+      self.create_by_name(name)
+    else 
+      self.find_by_name(name)
+    end
+  end
+    
+  def self.alphabetical()
+    self.sort_by{|x| x.name}
   end
 
 end
